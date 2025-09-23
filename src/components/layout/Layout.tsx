146e04e8943abfa076e834f5143e26793cdbceeb
Felipe <Felipe@gmail.com>
2025-09-22 23:01:26 -0300

    Starting navbar development

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,13 @@
 'use client'
 import React, { ReactNode } from 'react'
 import Sidebar from './components/Sidebar'
+import Navbar from './components/Navbar'
 
 function Layout({ children }: { children: ReactNode }) {
   return (
     <div className='flex'>
         <Sidebar />
+        <Navbar />
         <div>
             {children}
         </div>
---
+++ src/components/layout/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar(){
+    return(
+        <header>
+            <nav className="navbar">
+                <Link href="/">
+                    
+                </Link>
+
+                <ul className="nav-items">
+                    
+                </ul>
+            </nav>
+        </header>
+    )
+}
