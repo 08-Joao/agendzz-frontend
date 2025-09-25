85cb0570c56a5a14db694107876b9faf28785b68
Felipe Fialho <feliperfialho@gmail.com>
2025-09-25 18:40:21 -0300

    Dashboard tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
 'use client'
 import Layout from "@/components/layout/Layout";
+import { Button } from "@/components/ui/button"
 import ThemeSwitcher from "@/components/theme-switcher";
 import Image from "next/image";
+import Container from "@/components/ui/container";
 
 export default function Home() {
   return (
     <Layout>
-      Teste
+      <Container className="py-10">
+        <h1 className="text-3xl font-bold">Início</h1>
+        <p className="mt-4 text-gray-600">
+          Testando elementos do frontend.
+        </p>
+      </Container>
+      <div className="grid grid-rows-[20px_fr_20px] items-center justify-start">
+        <main className="flex flex-col gap-[32px] row-start-2 items-center justify-center">
+          <Button>
+            Agendamentos
+          </Button>
+        </main>
+      </div>
     </Layout>
   );
 }
