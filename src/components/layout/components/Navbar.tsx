d0d8ba77760f143702744e5568b2ce48996e4854
Felipe <Felipe@gmail.com>
2025-09-24 21:29:30 -0300

    Navbar items

--- src/components/layout/components/Navbar.tsx
+++ src/components/layout/components/Navbar.tsx
@@ -4,6 +4,8 @@ import { Bell } from '@solar-icons/react/ssr'
 import Link from 'next/link'
 import React, { useState } from 'react'
 
+
+
 // Generate initials from name
 const generateInitials = (name: string): string => {
     return name
@@ -18,6 +20,11 @@ export default function Navbar() {
   const userName = "João Silva"
   const [isUserSidebarOpen, setIsUserSidebarOpen] = useState(false)
 
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   const toggleUserSidebar = () => {
     setIsUserSidebarOpen(!isUserSidebarOpen)
   }
@@ -29,10 +36,21 @@ export default function Navbar() {
   return (
     <>
       <header className="sticky top-0 z-10 flex h-16 w-full items-center justify-between border-b bg-background/95 px-8 backdrop-blur">
-        <div>
-          <h1 className="text-xl font-bold">Dashboard</h1>
+         {/* Desktop Navigation */}
+        <div className="hidden md:flex space-x-4">
+          <Link href="/" className="text-black-300 hover:text-white">
+            Início
+          </Link>
+          <Link href="/about" className="text-black-300 hover:text-white">
+            Sobre
+          </Link>
+          <Link href="/services" className="text-black-300 hover:text-white">
+            Serviços
+          </Link>
+          <Link href="/contact" className="text-black-300 hover:text-white">
+            Contato
+          </Link>
         </div>
-        
         <div className="flex items-center space-x-4">
           <button className="relative rounded-full p-2 text-muted-foreground transition-colors hover:text-foreground focus:outline-none"aria-label="Ver notificações">
             <Bell size={20} />
